9f7aa65f4e079d58f4ade93374274bc837605f85
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-20 17:07:52 -0600

    jquery

--- html/js/js.js
+++ html/js/js.js
@@ -1,18 +1,20 @@
-const nombre = document.getElementById("nombre")
-            const mensaje = document.getElementById("mensaje")
-            const enviar = document.getElementById("enviar")
-            
-            enviar.addEventListener("click", async function(){
-                const nombreTxt = nombre.value
-                const mensajeTxt = mensaje.value
-            
-                let url = "https://demma-firmar-pdfs.cyclic.app/pdf"
-                url += "/?nombre=" + encodeURI(nombreTxt)
-                url += "&?mensaje=" + encodeURI(mensajeTxt)
-                const resp = await fetch(url, {method : 'GET',})
-                .then(function(response) {
-                    return response.json(); })
-                   .then(function(json) {return json})
-                console.log("response")
-                console.log(resp)
-        })
+const $ = require("jquery")
+
+const nombre = $("#nombre")
+const mensaje = $("#mensaje")
+const enviar = $("#enviar")
+
+enviar.addEventListener("click", async function(){
+    const nombreTxt = nombre.value
+    const mensajeTxt = mensaje.value
+
+    let url = "https://demma-firmar-pdfs.cyclic.app/pdf"
+    url += "/?nombre=" + encodeURI(nombreTxt)
+    url += "&?mensaje=" + encodeURI(mensajeTxt)
+    const resp = await fetch(url, {method : 'GET',})
+    .then(function(response) {
+        return response.json(); })
+        .then(function(json) {return json})
+    console.log("response")
+    console.log(resp)
+})
