8d2a98a19f1eb8848caf51eea15f553838e3a7ee
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-21 18:32:44 -0600

    await fetch

--- html/js/js.js
+++ html/js/js.js
@@ -10,7 +10,7 @@ const getPdf = async function(){
     let url = "https://demma-firmar-pdfs.cyclic.app/pdf"
     url += "/?nombre=" + encodeURI(nombreTxt)
     url += "&?mensaje=" + encodeURI(mensajeTxt)
-    const resp = fetch(url, {method : 'GET'})
+    const resp = await fetch(url, {method : 'GET'})
     .then(function(response) {
         return response.json(); })
         .then(function(json) {return json})
