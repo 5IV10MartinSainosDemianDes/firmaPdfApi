234633f4254cb86a537e57caf14d949f8c73f05c
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2022-11-23 14:11:05 -0600

    data log

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const signPdf = async function(body){
     console.log(body)
     console.log("end of body")
     const data = "name : "+body.name+"msg : "+body.msg
+    console.log("data",data)
     const resp = CryptoJS.SHA256(data).toString(CryptoJS.enc.Base64url)
     console.log("response",resp)
     return resp
